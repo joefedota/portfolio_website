f97b262a18c33b505576b8b9c10b812f15ed32f1
Joe Fedota <joefedota@berkeley.edu>
2020-05-26 21:53:38 -0700

    home and dummy page

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,8 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html', title='Home', img_path1 = "assets/cleantech.jpg")
+	return render_template('index.html', title='Home', img_path1 = "cleantech2-tn.png")
+
+@app.route('/cleantech')
+def cleantech():
+	return render_template('cleantech.html', title='Cleantech')
